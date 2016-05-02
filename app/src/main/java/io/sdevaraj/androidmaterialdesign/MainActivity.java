a656af90086f5817ccd26a31613967eb9cbd30cd
sdevaraj <santhoshkumar.devaraj@gmail.com>
2016-05-02 09:41:51 -0700

    Added Menu to toolbar. Added custom theme to the toolbar and menu.

--- app/src/main/java/io/sdevaraj/androidmaterialdesign/MainActivity.java
+++ app/src/main/java/io/sdevaraj/androidmaterialdesign/MainActivity.java
@@ -3,6 +3,7 @@ package io.sdevaraj.androidmaterialdesign;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
